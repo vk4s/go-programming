cfbf020dca56bf65ddbd25da43a3cb2c3e83b1cb
Vikash <vk4s.patel@gmail.com>
2024-04-04 08:22:36 +0000

    Create newDeck() function

--- projects/cards/deck.go
+++ projects/cards/deck.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-// create a new type of "deck", which is slice of strings
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
 
 // 'd' here is a 'this' of C++/JS and 'self' of Python
 func (d deck) print() {
--- projects/cards/main.go
+++ projects/cards/main.go
@@ -3,11 +3,6 @@ package main
 // import "fmt"
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "King of Spades")
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
