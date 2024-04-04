baf33f3e16615a5cf59864b4c3fdb1ef7216fc89
Vikash <vk4s.patel@gmail.com>
2024-04-04 07:42:36 +0000

    code cleanup

--- projects/cards/main.go
+++ projects/cards/main.go
@@ -3,30 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	welcome_message := "-- Welcome to the Game of Cards --"
-
-	fmt.Printf("%v\n%v\n", welcome_message,
-		separator_line("*", len(welcome_message)))
-
 	cards := []string{"Ace of Diamonds", newCard()}
 	cards = append(cards, "King of Spades")
 
-	fmt.Println("The participants are:")
-
 	for idx, element := range cards {
-		fmt.Printf("\t%v -> %v\n", idx, element)
+		fmt.Println(idx, element)
 	}
 }
 
 func newCard() string {
 	return "Five of Diamonds"
-}
-
-func separator_line(character string, times int) string {
-	// repeat a character multiple time
-	str := ""
-	for ; times >= 0; times-- {
-		str += character
-	}
-	return str
 }
