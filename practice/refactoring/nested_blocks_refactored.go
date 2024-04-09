f7c936848a82065377b4dc988818571b5acc1537
Vikash <v1kash@proton.me>
2024-04-09 13:11:55 +0000

    Nested if-else refactor (refactred code)

---
+++ practice/refactoring/nested_blocks_refactored.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func main() {
+	s1 := "Hello"
+	s2 := "World"
+
+	s, err := join(s1, s2, 20)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(s)
+}
+
+func join(s1, s2 string, max int) (string, error) {
+	if s1 == "" {
+		return "", errors.New("s1 is empty")
+	}
+	if s2 == "" {
+		return "", errors.New("s2 is empty")
+	}
+	concat, err := concatenate(s1, s2)
+	if err != nil {
+		return "", err
+	}
+	if len(concat) > max {
+		return concat[:max], nil
+	}
+	return concat, nil
+}
+
+func concatenate(s1 string, s2 string) (string, error) {
+	return s1 + s2, nil
+}
