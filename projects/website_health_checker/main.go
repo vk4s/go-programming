a10b8abea8839117a2b9ee0a6892cc9ea0b6d330
Vikash <v1kash@proton.me>
2024-04-11 08:43:03 +0000

    Project: website status checker

--- projects/website_health_checker/main.go
+++ projects/website_health_checker/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+/*
+This function checks status of a website and returns the `status code`.
+
+Parameters:
+  - url string
+
+Return:
+  - statusCode int
+  - error error
+*/
+func checkStatus(url string) (int, error) {
+
+	resp, err := http.Head(url)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return resp.StatusCode, nil
+}
+
 func main() {
+	websites := []string{
+		"http://google.com",
+		"http://wikipedia.org",
+		"http://golang.org",
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://coursedl.org",
+	}
+
+	for _, website := range websites {
+		statusCode, err := checkStatus(website)
+		if err != nil {
+			fmt.Println(website, "Error:", err)
+			os.Exit(1)
+		}
+		fmt.Println(website, statusCode)
+	}
 
 }
