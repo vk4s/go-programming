d27be88d930efec14d95eb990b8772a61d380d34
Vikash <v1kash@proton.me>
2024-04-30 10:19:44 +0000

    Add waitgroup

--- go-concurrency/goroutines/main.go
+++ go-concurrency/goroutines/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
 	words := []string{
 		"alpha",
 		"beta",
@@ -15,13 +17,30 @@ func main() {
 		"zeta",
 	}
 
+	// add the number of items to wait for
+	// use legth of slice/array for a dynamic number of items
+	wg.Add(len(words))
+
 	for i, v := range words {
-		go printSomething(fmt.Sprintf("%d: %s", i, v))
+		// pass a "REFERENCE" of the waitgroup object
+		go printSomething(fmt.Sprintf("%d: %s", i, v), &wg)
 	}
-	time.Sleep(1 * time.Second)
-	printSomething("Second go routine")
+	// time.Sleep(1 * time.Second)
+
+	wg.Add(1)
+	go printSomething("Second go routine", &wg)
+
+	// Wait till the go routine execution finishes
+	wg.Wait()
 }
 
-func printSomething(s string) {
+func printSomething(s string, wg *sync.WaitGroup) {
 	fmt.Println(s)
+	// Mark the execution as done.
+	// if you mark it before running all your code then
+	// there is no gaurantee that the code exdecuted after this point will be accessible
+	// and your main routine will wait for the response. It will (main routine) finish early
+	// and your program will exit.
+	// SO, ALWAYS PUT wg.Done() after all the statements are done executing in your go routine.
+	wg.Done()
 }
