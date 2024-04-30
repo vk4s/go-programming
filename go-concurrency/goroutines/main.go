c0804f85a964f643ac04a251ba32ced4ac2eb456
Vikash <v1kash@proton.me>
2024-04-30 09:54:56 +0000

    simple way to handle go routine

--- go-concurrency/goroutines/main.go
+++ go-concurrency/goroutines/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	go printSomething("First go routine")
+	words := []string{
+		"alpha",
+		"beta",
+		"delta",
+		"gamma",
+		"epsilon",
+		"zeta",
+	}
+
+	for i, v := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, v))
+	}
+	time.Sleep(1 * time.Second)
 	printSomething("Second go routine")
 }
 
